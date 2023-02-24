2c01b040eccad017038604bbbb26744d4cfaf705
Eugene Stashkevych <eugene.kevich@gmail.com>
2023-02-24 16:54:18 +0100

    Update search

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
   }
 
   const onUpdateSearch = (character) => {
-    setEnteredChar(character);
+    return setEnteredChar(character);
   };
 
   function countSpentMoney(data) {}
--- src/Search.js
+++ src/Search.js
@@ -7,15 +7,18 @@ export default function SearchPanel(props) {
   const onUpdateSearch = (event) => {
     const character = event.target.value;
     setEnteredChar(character);
-    props.onUpdateSearch(character);
+    props.updateSearch(character);
   };
   return (
-    <input
-      type="text"
-      placeholder="Find article"
-      className="form-control search-input"
-      value={enteredChar}
-      onChange={onUpdateSearch}
-    />
+    <>
+      <p>{console.log('props', props)}</p>
+      <input
+        type="text"
+        placeholder="Find article"
+        className="form-control search-input"
+        value={enteredChar}
+        onChange={onUpdateSearch}
+      />
+    </>
   );
 }
